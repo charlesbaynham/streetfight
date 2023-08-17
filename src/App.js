81e9ba298b855260ddfda8abaf1df5664eae212c
Charles Baynham <c.baynham@imperial.ac.uk>
2023-08-17 21:32:01 +0100

    Position crosshair over camera

--- src/App.js
+++ src/App.js
@@ -1,30 +1,64 @@
 import logo from './logo.svg';
+import crosshair from './crosshair.png';
 import './App.css';
+
 
 import Webcam from "react-webcam";
 
+
+const SCREEN_FILL_STYLES = {
+  position: "absolute",
+  height: "100vh",
+  width: "100vw",
+  left: "0",
+  top: "0",
+  objectFit: "cover"
+};
+
+// const videoConstraints = {
+//   width: 1280,
+//   height: 720,
+//   facingMode: "user"
+// };
+
+const WebcamCapture = () => (
+  <Webcam
+    audio={false}
+    // height={720}
+    screenshotFormat="image/jpeg"
+    // width={1280}
+    // videoConstraints={videoConstraints}
+    style={SCREEN_FILL_STYLES}
+  >
+    {/* {({ getScreenshot }) => (
+      <button
+        onClick={() => {
+          const imageSrc = getScreenshot()
+
+          console.log(imageSrc)
+        }}
+      >
+        Capture photo
+      </button>
+    )} */}
+  </Webcam>
+);
+
+const Crosshair =() => (
+  <img
+  src={crosshair}
+  style={SCREEN_FILL_STYLES}
+  />
+);
 
 function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        
+      <WebcamCapture />
+      <Crosshair />
 
-        <Webcam />;
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
