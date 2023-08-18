e713eacc66a42239258f34ea174fdf92a650ad68
Charles Baynham <c.baynham@imperial.ac.uk>
2023-08-18 09:48:59 +0100

    Flaky nonsense

--- flake.nix
+++ flake.nix
@@ -6,22 +6,36 @@
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-      pkgs = nixpkgs.legacyPackages.${system};
-      reqs = [
-        pkgs.nodejs
-      ];
-      
-      in {
-        devShell = 
+        pkgs = nixpkgs.legacyPackages.${system};
+        reqs = [
+          pkgs.nodejs
+        ];
+
+      in
+      {
+        devShell =
           pkgs.mkShell {
-            name="devShell";
-            buildInputs=reqs;
+            name = "devShell";
+            buildInputs = reqs;
           };
-        
-        # apps = rec {
-        #   hello = flake-utils.lib.mkApp { drv = self.packages.${system}.hello; };
-        #   default = hello;
-        # };
+
+        apps = rec {
+          start = flake-utils.lib.mkApp {
+            drv = (pkgs.writeShellScriptBin "script" ''
+              export PATH=${pkgs.lib.makeBinPath reqs}:$PATH
+
+              exec npm run start
+            '');
+          };
+          deploy = flake-utils.lib.mkApp {
+            drv = (pkgs.writeShellScriptBin "script" ''
+              export PATH=${pkgs.lib.makeBinPath reqs}:$PATH
+
+              exec npm run deploy
+            '');
+          };
+          default = start;
+        };
       }
     );
 }
