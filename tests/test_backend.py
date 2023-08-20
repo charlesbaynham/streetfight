32f41d3b016b67462b80aa75e17ac9cb88037fa7
Charles Baynham <c.baynham@imperial.ac.uk>
2023-08-20 21:06:44 +0100

    I forgot how useful good tests are

--- tests/test_backend.py
+++ tests/test_backend.py
@@ -9,3 +9,9 @@ def test_read_main(api_client):
     response = api_client.get("/api/hello")
     assert response.status_code == 200
     assert response.json() == {"msg": "Hello world!"}
+
+
+def test_user_info(api_client):
+    response = api_client.get("/api/user_info")
+    assert response.status_code == 200
+    # assert response.json() == {"msg": "Hello world!"}
