3b33d23947066bb3d5e242919741b516d1bc5995
Charles Baynham <c.baynham@imperial.ac.uk>
2023-08-20 17:50:55 +0100

    Bare admin mode

---
+++ react-ui/src/AdminMode.js
@@ -0,0 +1,13 @@
+import React, { useCallback, useState } from 'react';
+
+
+
+export default function AdminMode() {
+    return (
+        <>
+            <h1>Admin mode</h1>
+
+            <p>Welcome to admin mode. I hope you're not a cheater...</p>
+        </>
+    );
+}
--- react-ui/src/index.js
+++ react-ui/src/index.js
@@ -2,20 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import './index.css';
 
-import reportWebVitals from './reportWebVitals';
-import ShootMode from './ShootMode';
+import AdminMode from './AdminMode';
 
 export default function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="*" element={<ShootMode />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+
+
+    <BrowserRouter>
+      <Routes>
+        <Route path="*" element={<AdminMode />} />
+      </Routes>
+    </BrowserRouter>
+
   );
 }
 
@@ -25,8 +24,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
