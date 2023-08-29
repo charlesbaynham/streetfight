4749cc5a58a69c273ce7ef9a764f3a6eb62c3d6c
Charles Baynham <c.baynham@imperial.ac.uk>
2023-08-29 08:40:23 +0100

    Make valid dev teams

--- backend/reset_db.py
+++ backend/reset_db.py
@@ -14,10 +14,10 @@ logger = logging.getLogger(__name__)
 
 SAMPLE_GAME_ID = UUID("a47c0fcf-67bd-4c91-a83b-1ac6c3d8fd43")
 
-SAMPLE_TEAM_IDS = [
-    UUID("890e8e0a-575b-4df6-85ee-eb7e6656085d"),
-    UUID("c6f6f9f9-3a90-469e-a344-54a2cf33f4e7"),
-    UUID("01f56942-5a4e-48c0-bc9d-b037a0a22653"),
+SAMPLE_TEAMS = [
+    (UUID("890e8e0a-575b-4df6-85ee-eb7e6656085d"), "Team A"),
+    (UUID("c6f6f9f9-3a90-469e-a344-54a2cf33f4e7"), "Team B"),
+    (UUID("01f56942-5a4e-48c0-bc9d-b037a0a22653"), "Team C"),
 ]
 
 
@@ -36,8 +36,8 @@ def reset_database():
         session = database.Session()
         g = Game(id=SAMPLE_GAME_ID)
         session.add(g)
-        for team_id in SAMPLE_TEAM_IDS:
-            session.add(Team(id=team_id, game=g))
+        for team_id, team_name in SAMPLE_TEAMS:
+            session.add(Team(id=team_id, game=g, name=team_name))
 
         session.commit()
 
