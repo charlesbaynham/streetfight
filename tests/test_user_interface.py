1dc9a152bf80642f22405cf67690d85f8247d854
Charles Baynham <c.baynham@imperial.ac.uk>
2023-09-02 19:16:32 +0100

    Test user cannot shoot while dead

--- tests/test_user_interface.py
+++ tests/test_user_interface.py
@@ -38,12 +38,37 @@ def test_user_shots_respect_ammo(db_session, team_factory, user_factory):
 
     UserInterface(user_id).join_team(team_id)
 
-    # Give the user a bullet
+    # Give the user some bullets
     user = db_session.query(User).filter_by(id=user_id).first()
-    user.num_bullets = 1
+    user.num_bullets = 3
+    db_session.commit()
+
+    for _ in range(3):
+        UserInterface(user_id).submit_shot(test_data)
+
+    with pytest.raises(HTTPException):
+        UserInterface(user_id).submit_shot(test_data)
+
+
+def test_user_cannot_shoot_when_dead(db_session, team_factory, user_factory):
+    from base64 import b64encode
+
+    team_id = team_factory()
+    user_id = user_factory()
+
+    test_data = str(b64encode(b"123"))
+
+    UserInterface(user_id).join_team(team_id)
+
+    # Give the user 3 bullets
+    user = db_session.query(User).filter_by(id=user_id).first()
+    user.num_bullets = 3
     db_session.commit()
 
     UserInterface(user_id).submit_shot(test_data)
+
+    # Kill them
+    UserInterface(user_id).kill()
 
     with pytest.raises(HTTPException):
         UserInterface(user_id).submit_shot(test_data)
