75cf19ad8e44c35f527ef0af2a7e5dc76b2526c5
Charles Baynham <c.baynham@imperial.ac.uk>
2023-09-02 18:44:44 +0100

    Add failing test

--- tests/test_user_interface.py
+++ tests/test_user_interface.py
@@ -1,2 +1,8 @@
-# def test_can_join_game():
-#     join_game
+from uuid import uuid4 as uuid
+
+from backend.user_interface import UserInterface
+
+
+def test_can_join_game(user_factory):
+    user_id = user_factory()
+    UserInterface(user_id=user_id).join_game(uuid())
