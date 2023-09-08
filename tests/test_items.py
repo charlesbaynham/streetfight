7ddd6ebc17d203b46368025e7d51edd407d214be
Charles Baynham <c.baynham@imperial.ac.uk>
2023-09-08 18:13:35 +0100

    Fail tests until the feature is actually done

--- tests/test_items.py
+++ tests/test_items.py
@@ -1,46 +1,37 @@
 import pytest
 
 
-@pytest.mark.xfail
 def test_can_collect_item():
     raise NotImplementedError
 
 
-@pytest.mark.xfail
 def test_item_appears_in_database():
     raise NotImplementedError
 
 
-@pytest.mark.xfail
 def test_cannot_collect_invalid_item():
     raise NotImplementedError
 
 
-@pytest.mark.xfail
 def test_cannot_collect_item_twice():
     raise NotImplementedError
 
 
-@pytest.mark.xfail
 def test_can_generate_valid_item():
     raise NotImplementedError
 
 
-@pytest.mark.xfail
 def test_collecting_armour_when_alive():
     raise NotImplementedError
 
 
-@pytest.mark.xfail
 def test_collecting_armour_when_dead():
     raise NotImplementedError
 
 
-@pytest.mark.xfail
 def test_collecting_ammo():
     raise NotImplementedError
 
 
-@pytest.mark.xfail
 def test_collecting_revive():
     raise NotImplementedError
