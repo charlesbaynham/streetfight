7e81aa5763cd7d8c31bec5542eff856b6d9229e6
Charles Baynham <c.baynham@imperial.ac.uk>
2023-09-09 23:07:02 +0100

    Revert "Switch item collection to query params so I can embed URLs into the qr code"
    I don't need to do this - React will be handling this anyway
    
    This reverts commit d9f7cca964f604b697fdf1766f3658188d82eefd.

--- backend/main.py
+++ backend/main.py
@@ -121,12 +121,17 @@ async def join_game(
     return UserInterface(user_id).join_game(game_id)
 
 
+class _EncodedItem(BaseModel):
+    data: str
+
+
 @router.post("/collect_item")
 async def collect_item(
-    d: str,
+    encoded_item: _EncodedItem,
     user_id=Depends(get_user_id),
 ):
-    return UserInterface(user_id).collect_item(d)
+
+    return UserInterface(user_id).collect_item(encoded_item.data)
 
 
 @router.get("/get_hash")
