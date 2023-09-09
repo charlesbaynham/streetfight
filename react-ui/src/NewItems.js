527a5dfaead5380629ea168c2e4ce7484f0043bf
Charles Baynham <c.baynham@imperial.ac.uk>
2023-09-09 19:43:26 +0100

    Slightly less hacky

--- react-ui/src/NewItems.js
+++ react-ui/src/NewItems.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useCallback, useState, useRef } from 'react';
 
 import QRCode from "react-qr-code";
 
@@ -7,36 +7,59 @@ import { sendAPIRequest } from './utils';
 export default function NewItems() {
     const [data, setData] = useState(null);
 
-    const getAmmo = useCallback(() => {
+    const getItem = useCallback((item_type, item_num) => {
         const item_data = {
-            num: 123
+            num: item_num
         };
 
         const callback = (d) => {
-            console.log("New item:")
+            console.log(`New ${item_type}/${item_num}:`)
             console.log(d)
             setData(d)
         };
 
         sendAPIRequest(
             "admin_make_new_item",
-            { "item_type": "ammo" },
+            { "item_type": item_type },
             'POST',
             callback,
             item_data
         );
     }, [setData]);
 
-    useEffect(getAmmo, []);
+    const itemTypeRef = useRef();
+    const itemNumRef = useRef();
 
     return <>
+
+        <>
+            <span>Type:</span>
+
+            <select ref={itemTypeRef} >
+                <option value="ammo">Ammo</option>
+                <option value="medkit">Medkit</option>
+                <option value="armour">Armour</option>
+            </select>
+
+            <span>Num:</span>
+            <input ref={itemNumRef} type="number" />
+
+            <button onClick={() => {
+                getItem(itemTypeRef.current.value, parseInt(itemTypeRef.current.value))
+            }
+            }
+            >Generate</button>
+
+        </>
+        <br />
+
         {
             data ?
                 <QRCode value={data} />
                 : null
         }
 
-        <button onClick={getAmmo}>New</button>
+
 
     </>
 }
