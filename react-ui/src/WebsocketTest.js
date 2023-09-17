4f0671b31c77ff8827326cd58b45a965ce9668ec
Charles Baynham <c.baynham@imperial.ac.uk>
2023-09-17 15:24:20 +0100

    Urrr yup, I need to specify https...

--- react-ui/src/WebsocketTest.js
+++ react-ui/src/WebsocketTest.js
@@ -7,7 +7,7 @@ function WebsocketTest() {
 
     useEffect(() => {
         // Establish a WebSocket connection
-        const newWs = new WebSocket('ws://localhost:3000/api/ws'); // Replace with your WebSocket server URL
+        const newWs = new WebSocket('wss://localhost:3000/api/ws');
 
         newWs.onopen = () => {
             console.log('WebSocket connected');
