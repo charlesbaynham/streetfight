5d00c0f28e7b29fd533538576ec46bab441324a5
Charles Baynham <c.baynham@imperial.ac.uk>
2023-09-19 21:54:13 +0100

    Add another row

--- react-ui/src/NoNameView.js
+++ react-ui/src/NoNameView.js
@@ -2,6 +2,8 @@ import { useCallback, useState } from "react";
 import { sendAPIRequest } from "./utils";
 
 import returnIcon from './return.svg';
+import actionNotDone from './hand-pointer-solid.svg';
+import actionDone from './check-solid.svg';
 import styles from './NoNameView.module.css';
 
 function NoNameView({ callback = null }) {
@@ -19,19 +21,29 @@ function NoNameView({ callback = null }) {
 
     return (
         <div className={styles.outerContainer}>
-            <div className={styles.inputHolder}>
-                <input
-                    className={styles.nameInput}
-                    value={nameBoxValue}
-                    onChange={(e) => { setNameBoxValue(e.target.value) }}
-                    onKeyDown={handleKeyDown}
-                    placeholder="Enter your name..."
-                />
-                <button
-                    className={styles.nameButton}
-                    onClick={setUserName}>
-                    <img src={returnIcon} alt="" />
-                </button>
+            <div className={styles.innerContainer}>
+                <div className={styles.stackedItem}>
+                    <input
+                        className={styles.nameInput}
+                        value={nameBoxValue}
+                        onChange={(e) => { setNameBoxValue(e.target.value) }}
+                        onKeyDown={handleKeyDown}
+                        placeholder="Enter your name..."
+                    />
+                    <button
+                        className={styles.nameButton}
+                        onClick={setUserName}>
+                        <img src={returnIcon} alt="" />
+                    </button>
+                </div>
+                <div className={styles.stackedItem}>
+                    <p>Do something important</p>
+                    <button
+                        className={styles.actionButton}
+                    >
+                        <img src={actionNotDone} alt="" />
+                    </button>
+                </div>
             </div>
         </div>
     );
