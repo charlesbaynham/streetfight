df5683fcc116c6a27a29b6f0d2b0fa4e5d167a07
Charles Baynham <c.baynham@imperial.ac.uk>
2023-09-20 20:33:27 +0100

    Fix the hash-based tests which have been broken since I disabled the hash...

--- tests/test_ticker.py
+++ tests/test_ticker.py
@@ -37,27 +37,6 @@ def test_ticker_filters_correctly_order(ticker):
 
 
 @pytest.mark.asyncio
-async def test_ticker_game_hash(ticker):
-    await ticker.get_hash()
-
-
-@pytest.mark.asyncio
-async def test_ticker_game_hash_changes(ticker):
-    starting_hash = await ticker.get_hash()
-    ticker.post_message("Hello")
-    assert starting_hash != await ticker.get_hash()
-
-
-@pytest.mark.asyncio
-async def test_ticker_messages_via_user(user_in_team):
-    ticker = UserInterface(user_in_team).get_ticker()
-
-    starting_hash = await ticker.get_hash()
-    ticker.post_message("Hello")
-    assert starting_hash != await ticker.get_hash()
-
-
-@pytest.mark.asyncio
 async def test_ticker_messages_via_user_outside_team(user_factory):
     ticker = UserInterface(user_factory()).get_ticker()
     assert ticker is None
@@ -68,11 +47,6 @@ def test_api_query_ticker_outside_team(api_client):
     assert response.ok
     messages = response.json()
 
-    response = api_client.get("/api/ticker_hash")
-    assert response.ok
-    new_hash = response.json()
-
-    assert new_hash == 0
     assert messages == []
 
 
@@ -84,21 +58,12 @@ def test_api_query_ticker_inside_team(api_client, api_user_id, team_factory):
     assert response.ok
     messages = response.json()
 
-    response = api_client.get("/api/ticker_hash")
-    assert response.ok
-    new_hash = response.json()
-
-    assert new_hash != 0
-    assert messages == []
+    assert len(messages) == 1  # Just the "joined team" message
 
 
 def test_api_query_ticker_messages(api_client, api_user_id, team_factory):
     team_id = team_factory()
     AdminInterface().add_user_to_team(api_user_id, team_id)
-
-    response = api_client.get("/api/ticker_hash")
-    assert response.ok
-    old_hash = response.json()
 
     UserInterface(api_user_id).get_ticker().post_message("hello")
 
@@ -106,9 +71,5 @@ def test_api_query_ticker_messages(api_client, api_user_id, team_factory):
     assert response.ok
     messages = response.json()
 
-    response = api_client.get(f"/api/ticker_hash?known_ticker_hash={old_hash}")
-    assert response.ok
-    new_hash = response.json()
-
-    assert new_hash != old_hash
-    assert messages == ["hello"]
+    assert len(messages) == 2
+    assert "hello" in messages
