d3c851834b02ecbbfc6350ce419523c5e4e8d3b8
Charles Baynham <c.baynham@imperial.ac.uk>
2023-09-20 23:18:16 +0100

    Fix tests

--- tests/test_user_interface.py
+++ tests/test_user_interface.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from uuid import uuid4 as uuid
 
 import pytest
@@ -5,6 +6,11 @@ from fastapi.exceptions import HTTPException
 
 from backend.model import User
 from backend.user_interface import UserInterface
+
+
+@pytest.fixture
+def test_image():
+    return Path(__file__, "../sample_base64_image.txt").resolve().read_text()
 
 
 def test_can_join_new_team(user_factory):
@@ -28,13 +34,9 @@ def test_cannot_join_new_team_if_multiple_games(game_factory, user_factory):
         UserInterface(user_id=user_id).join_team(uuid())
 
 
-def test_user_shots_respect_ammo(db_session, team_factory, user_factory):
-    from base64 import b64encode
-
+def test_user_shots_respect_ammo(db_session, team_factory, user_factory, test_image):
     team_id = team_factory()
     user_id = user_factory()
-
-    test_data = str(b64encode(b"123"))
 
     UserInterface(user_id).join_team(team_id)
 
@@ -44,19 +46,17 @@ def test_user_shots_respect_ammo(db_session, team_factory, user_factory):
     db_session.commit()
 
     for _ in range(3):
-        UserInterface(user_id).submit_shot(test_data)
+        UserInterface(user_id).submit_shot(test_image)
 
     with pytest.raises(HTTPException):
-        UserInterface(user_id).submit_shot(test_data)
+        UserInterface(user_id).submit_shot(test_image)
 
 
-def test_user_cannot_shoot_when_dead(db_session, team_factory, user_factory):
-    from base64 import b64encode
-
+def test_user_cannot_shoot_when_dead(
+    db_session, team_factory, user_factory, test_image
+):
     team_id = team_factory()
     user_id = user_factory()
-
-    test_data = str(b64encode(b"123"))
 
     UserInterface(user_id).join_team(team_id)
 
@@ -65,13 +65,13 @@ def test_user_cannot_shoot_when_dead(db_session, team_factory, user_factory):
     user.num_bullets = 3
     db_session.commit()
 
-    UserInterface(user_id).submit_shot(test_data)
+    UserInterface(user_id).submit_shot(test_image)
 
     # Kill them
     UserInterface(user_id).kill()
 
     with pytest.raises(HTTPException):
-        UserInterface(user_id).submit_shot(test_data)
+        UserInterface(user_id).submit_shot(test_image)
 
 
 def test_can_give_health(user_in_team):
