101f5b8fc996a2222abf3a47833b501ea373661b
Charles Baynham <c.baynham@imperial.ac.uk>
2023-09-20 21:58:03 +0100

    Write the test properly

--- tests/test_image_processing.py
+++ tests/test_image_processing.py
@@ -4,6 +4,8 @@ from backend.image_processing import draw_cross_on_image
 
 
 def test_image_processsing():
-    test_image_string = Path(__file__, "../sample_base64_image.txt").read_text()
+    test_image_string = (
+        Path(__file__, "../sample_base64_image.txt").resolve().read_text()
+    )
 
     draw_cross_on_image(test_image_string)
