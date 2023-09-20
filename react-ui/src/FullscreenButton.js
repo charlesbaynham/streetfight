757ba6fba0194ce4a4b62d4139d2666a68ce34d0
Charles Baynham <c.baynham@imperial.ac.uk>
2023-09-20 14:33:06 +0100

    Format

--- react-ui/src/FullscreenButton.js
+++ react-ui/src/FullscreenButton.js
@@ -3,9 +3,14 @@ import styles from './FullscreenButton.module.css';
 import icon from './fullscreen-icon.svg';
 
 const FullscreenButton = ({ handle }) => (
-    <button className={styles.button} onClick={handle.enter}>
-        <img src={icon} alt="Fullscreen" />
-    </button>
+    <>
+        <button className={styles.button} onClick={handle.enter}>
+            <img src={icon} alt="Fullscreen" />
+        </button>
+        <div className={styles.fullscreenHint}>
+            Go fullscreen!
+        </div>
+    </>
 )
 
 export default FullscreenButton;
