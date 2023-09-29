d0e8c3c38eb3a2b96fff33cde4fe12a0dea721d1
Charles Baynham <c.baynham@imperial.ac.uk>
2023-09-29 22:17:58 +0100

    Animate gun pickup

--- react-ui/src/BulletCount.js
+++ react-ui/src/BulletCount.js
@@ -10,6 +10,10 @@ import medkit from './images/medkit.svg';
 import bullet from './images/bullet.svg';
 import armour from './images/helmet.svg';
 import cross from './images/cross.svg';
+import gun_11 from './images/gun_11.svg';
+import gun_26 from './images/gun_26.svg';
+import gun_36 from './images/gun_36.svg';
+import gun_default from './images/gun_default.svg';
 
 
 const make_n_images = (n, image) => Array(n).fill().map((_, i) =>
@@ -24,6 +28,12 @@ export default function BulletCount({ user }) {
     const [showArmourAnim, setShowArmourAnim] = useState(false);
     const [showMedpackAnim, setShowMedpackAnim] = useState(false);
 
+    const [showGun11, setShowGun11] = useState(false);
+    const [showGun26, setShowGun26] = useState(false);
+    const [showGun36, setShowGun36] = useState(false);
+
+    const anyActive = showBulletAnim | showArmourAnim | showMedpackAnim | showGun11 | showGun26 | showGun36;
+
     useEffect(() => {
         var timeoutHandle = null;
 
@@ -32,10 +42,18 @@ export default function BulletCount({ user }) {
             setShowArmourAnim(user.hit_points > previousUser.hit_points && previousUser.hit_points > 0);
             setShowMedpackAnim(user.hit_points === 1 && previousUser.hit_points === 0);
 
+            const collectedGun = (user.shot_damage != previousUser.shot_damage) | (user.shot_timeout != previousUser.shot_timeout);
+            setShowGun11(collectedGun && user.shot_damage === 1 && user.shot_timeout === 1)
+            setShowGun26(collectedGun && user.shot_damage === 2 && user.shot_timeout === 6)
+            setShowGun36(collectedGun && user.shot_damage === 3 && user.shot_timeout === 6)
+
             timeoutHandle = setTimeout(() => {
                 setShowBulletAnim(false)
                 setShowArmourAnim(false)
                 setShowMedpackAnim(false)
+                setShowGun11(false)
+                setShowGun26(false)
+                setShowGun36(false)
             }, 3000)
         }
 
@@ -67,9 +85,12 @@ export default function BulletCount({ user }) {
             <TemporaryOverlay img={armour} appear={showArmourAnim} />
             <TemporaryOverlay img={medkit} appear={showMedpackAnim} />
 
+            <TemporaryOverlay img={gun_11} appear={showGun11} />
+            <TemporaryOverlay img={gun_26} appear={showGun26} />
+            <TemporaryOverlay img={gun_36} appear={showGun36} />
 
             <CollectItemFromQueryParam enabled={
-                !showBulletAnim && !showArmourAnim && !showMedpackAnim
+                !anyActive
             } />
         </div>
 
