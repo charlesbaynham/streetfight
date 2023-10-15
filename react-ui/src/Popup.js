8be88134a6746fdd353b923077dbb622a4005b40
Charles Baynham <c.baynham@imperial.ac.uk>
2023-10-15 13:07:44 +0100

    Darken background

--- react-ui/src/Popup.js
+++ react-ui/src/Popup.js
@@ -6,34 +6,36 @@ import styles from './Popup.module.css'
 import buttonImg from './images/exit-button.svg'
 
 const variants = {
-    // open: { opacity: 1, scale: 1 },
-    // closed: { opacity: 0, scale: 0 },
     open: { opacity: 1 },
     closed: { opacity: 0 },
 }
 
 function Popup({ children, visible, setVisible }) {
-    const out = <motion.div
-        className={styles.outerContainer}
-        initial="closed"
-        animate={visible ? "open" : "closed"}
-        transition={{ duration: 0.5 }
-        }
-        variants={variants}
-        exit="closed"
-    >
-        <button
-            className={styles.exitButton}
-            onClick={() => {
-                setVisible(false)
-            }}
-        >
-            <img src={buttonImg} alt="" />
-        </button>
-        <div className={styles.innerContainer} >
-            {children}
+    const out = (
+        <div className={styles.fullscreenContainer}>
+            <motion.div
+                className={styles.outerContainer}
+                initial="closed"
+                animate={visible ? "open" : "closed"}
+                transition={{ duration: 0.5 }
+                }
+                variants={variants}
+                exit="closed"
+            >
+                <button
+                    className={styles.exitButton}
+                    onClick={() => {
+                        setVisible(false)
+                    }}
+                >
+                    <img src={buttonImg} alt="" />
+                </button>
+                <div className={styles.innerContainer} >
+                    {children}
+                </div>
+            </motion.div>
         </div>
-    </motion.div>
+    )
 
     return <AnimatePresence>
         {visible ? out : null}
