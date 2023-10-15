a259603ac48eabc72675a0139c3b1ae625252aa2
Charles Baynham <c.baynham@imperial.ac.uk>
2023-10-15 12:56:24 +0100

    Animate presence

--- react-ui/src/Popup.js
+++ react-ui/src/Popup.js
@@ -1,18 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { motion, AnimatePresence } from "framer-motion"
 
 import styles from './Popup.module.css'
 import buttonImg from './images/exit-button.svg'
 
-function Popup({ children }) {
+const variants = {
+    // open: { opacity: 1, scale: 1 },
+    // closed: { opacity: 0, scale: 0 },
+    open: { opacity: 1 },
+    closed: { opacity: 0 },
+}
+
+function Popup({ children, visible, setVisible }) {
+    const [animate, setAnimate] = useState("open");
+
     return (
-        <div className={styles.outerContainer} >
-            <button className={styles.exitButton}>
+        <motion.div
+            className={styles.outerContainer}
+            initial="closed"
+            animate={visible ? "open" : "closed"}
+            transition={{ duration: 0.5 }}
+            variants={variants}
+            exit="closed"
+        >
+            <button
+                className={styles.exitButton}
+                onClick={() => {
+                    setAnimate("closed")
+                    setVisible(false)
+                }}
+            >
                 <img src={buttonImg} alt="" />
             </button>
             <div className={styles.innerContainer} >
                 {children}
             </div>
-        </div >
+        </motion.div >
     );
 }
 
--- react-ui/src/TestPage.js
+++ react-ui/src/TestPage.js
@@ -1,11 +1,20 @@
+import { useEffect, useState } from "react";
 import Popup from "./Popup";
 import Scoreboard from "./Scoreboard";
 
 import streetImage from "./images/test-street-image.jpg"
 
 const TestPage = () => {
+    const [visible, setVisible] = useState(false);
+
     return (
         <>
+            <button onClick={() => {
+                setVisible(!visible)
+            }}>
+                Toggle
+            </button >
+
             <img
                 src={streetImage}
                 style={{
@@ -13,9 +22,10 @@ const TestPage = () => {
                     width: "100vw"
                 }}
             />
-            <Popup>
+
+            < Popup visible={visible} setVisible={setVisible} >
                 <Scoreboard />
-            </Popup>
+            </Popup >
         </>
     )
 };
