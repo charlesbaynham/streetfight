02e629e363ab94e6ae0ba3f761a8c9325aa3ef12
Charles Baynham <c.baynham@imperial.ac.uk>
2023-10-16 21:57:14 +0100

    Autoupdate

--- react-ui/src/Scoreboard.js
+++ react-ui/src/Scoreboard.js
@@ -1,5 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { sendAPIRequest } from './utils';
+import UpdateListener from './UpdateListener';
 
 import styles from './Scoreboard.module.css'
 
@@ -8,7 +9,7 @@ function Scoreboard() {
     const [tableHeader, setTableHeaders] = useState(null);
     const [tableContents, setTableContents] = useState(null);
 
-    useEffect(() => {
+    const update = useCallback(() => {
         sendAPIRequest("get_scoreboard")
             .then(async response => {
                 if (!response.ok)
@@ -18,20 +19,32 @@ function Scoreboard() {
                 setTableHeaders(data.headers);
                 setTableContents(data.table);
             });
-    }, [setTableHeaders, setTableContents])
+    }, [setTableHeaders, setTableContents]);
 
-    return ((tableHeader !== null & tableContents !== null) ?
-        <table className={styles.scoretable}>
-            <tr>
-                {tableHeader.map((e, i) => <th key={i}>{e}</th>)}
-            </tr>
-            {tableContents.map((row, i_row) =>
-                <tr key={i_row}>
-                    {row.map((e, i) => <td key={i}>{e}</td>)}
-                </tr>
-            )}
-        </table>
-        : null);
+    // Update scoreboard on load
+    useEffect(update, [update])
+
+    return <>
+        {
+            (tableHeader !== null & tableContents !== null) ?
+                <table className={styles.scoretable}>
+                    <tr>
+                        {tableHeader.map((e, i) => <th key={i}>{e}</th>)}
+                    </tr>
+                    {tableContents.map((row, i_row) =>
+                        <tr key={i_row}>
+                            {row.map((e, i) => <td key={i}>{e}</td>)}
+                        </tr>
+                    )}
+                </table>
+                : null
+        }
+        {/* Also update scoreboard when the ticker updates */}
+        <UpdateListener
+            update_type="ticker"
+            callback={update}
+        />
+    </>;
 }
 
 export default Scoreboard;
