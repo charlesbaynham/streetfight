453aef38ad83079c28a3f955edc103e53bbfa3dd
Charles Baynham <c.baynham@imperial.ac.uk>
2024-09-14 18:09:30 +0100

    Hide fullscreen button if the app is installed

--- react-ui/src/FullscreenButton.js
+++ react-ui/src/FullscreenButton.js
@@ -4,10 +4,17 @@ import styles from "./FullscreenButton.module.css";
 
 import fullscreenImg from "./images/fullscreen-icon.svg";
 import hintImg from "./images/better-in-fullscreen.svg";
+import { prepareInstallPrompt, showInstallPrompt, isStandalone } from "./AddToHomeScreen";
 import { useCallback, useEffect, useState } from "react";
 
 function FullscreenButton({ handle, isFullscreen, keepHintVisible = false }) {
   const [hintHidden, setHintHidden] = useState(false);
+  const [isInstalled, setIsInstalled] = useState(false);
+
+  useEffect(() => {
+    prepareInstallPrompt();
+    setIsInstalled(isStandalone());
+  }, [])
 
   useEffect(() => {
     if (keepHintVisible) return;
@@ -26,24 +33,26 @@ function FullscreenButton({ handle, isFullscreen, keepHintVisible = false }) {
     else handle.enter();
   }, [handle, isFullscreen]);
 
-  return (
-    <motion.div className={styles.buttonContainer}>
-      <button onClick={toggleFullscreen}>
-        <img src={fullscreenImg} alt="Fullscreen" />
-      </button>
-      <motion.img
-        className={styles.fullscreenHint}
-        animate={{
-          opacity: hintHidden ? 0 : 1,
-        }}
-        transition={{
-          duration: 3,
-        }}
-        src={hintImg}
-        alt="This app is better in fullscreen"
-      />
-    </motion.div>
-  );
+  return <>
+    {isInstalled ? null :
+      <motion.div className={styles.buttonContainer}>
+        <button onClick={toggleFullscreen}>
+          <img src={fullscreenImg} alt="Fullscreen" />
+        </button>
+        <motion.img
+          className={styles.fullscreenHint}
+          animate={{
+            opacity: hintHidden ? 0 : 1,
+          }}
+          transition={{
+            duration: 3,
+          }}
+          src={hintImg}
+          alt="This app is better in fullscreen"
+        />
+      </motion.div>
+    }
+  </>;
 }
 
 export default FullscreenButton;
