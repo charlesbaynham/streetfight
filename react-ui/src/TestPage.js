61c3b044fc2547064b000297eaadee0ef9522fae
Charles Baynham <charles.baynham@gmail.com>
2024-09-17 08:10:01 +0100

    Navigation test (#38)
    * Test location API
    
    * Very simple nav test
    
    * Prettified Code!
    
    ---------
    
    Co-authored-by: charlesbaynham <charlesbaynham@users.noreply.github.com>

--- react-ui/src/TestPage.js
+++ react-ui/src/TestPage.js
@@ -1,12 +1,40 @@
-import React from "react";
-import SSEComponent from "./SSETest";
+import React, { useEffect, useState } from "react";
 
 const TestPage = () => {
+  const [loc, setLoc] = useState(null);
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      setLoc("Loading...");
+
+      const watchId = navigator.geolocation.watchPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+          setLoc(
+            `Timestamp: ${position.timestamp}, Latitude: ${latitude}, Longitude: ${longitude}, Accuracy: ${position.coords.accuracy}`,
+          );
+        },
+        (error) => {
+          console.error("Error watching position:", error);
+          setLoc("Error getting position");
+        },
+      );
+
+      return () => {
+        navigator.geolocation.clearWatch(watchId);
+      };
+    } else {
+      console.error("Geolocation is not supported by this browser.");
+    }
+  }, []);
+
   return (
     <div>
       <h1>This is a test</h1>
 
-      <SSEComponent />
+      <h3>Your location:</h3>
+
+      {loc}
     </div>
   );
 };
