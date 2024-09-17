71e1a55a128964df4d3269300ad9e1b733e1383f
Charles Baynham <c.baynham@imperial.ac.uk>
2024-09-17 08:11:59 +0100

    Revert "Navigation test (#38)"
    This reverts commit 61c3b044fc2547064b000297eaadee0ef9522fae.

--- react-ui/src/TestPage.js
+++ react-ui/src/TestPage.js
@@ -1,40 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+import SSEComponent from "./SSETest";
 
 const TestPage = () => {
-  const [loc, setLoc] = useState(null);
-
-  useEffect(() => {
-    if (navigator.geolocation) {
-      setLoc("Loading...");
-
-      const watchId = navigator.geolocation.watchPosition(
-        (position) => {
-          const { latitude, longitude } = position.coords;
-          setLoc(
-            `Timestamp: ${position.timestamp}, Latitude: ${latitude}, Longitude: ${longitude}, Accuracy: ${position.coords.accuracy}`,
-          );
-        },
-        (error) => {
-          console.error("Error watching position:", error);
-          setLoc("Error getting position");
-        },
-      );
-
-      return () => {
-        navigator.geolocation.clearWatch(watchId);
-      };
-    } else {
-      console.error("Geolocation is not supported by this browser.");
-    }
-  }, []);
-
   return (
     <div>
       <h1>This is a test</h1>
 
-      <h3>Your location:</h3>
-
-      {loc}
+      <SSEComponent />
     </div>
   );
 };
