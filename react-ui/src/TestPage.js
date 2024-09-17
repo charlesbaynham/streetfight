cd3fbb9af581c43aa28317b2b6ea3636ccf235a9
Charles Baynham <c.baynham@imperial.ac.uk>
2024-09-17 07:49:16 +0100

    Test location API

--- react-ui/src/TestPage.js
+++ react-ui/src/TestPage.js
@@ -1,12 +1,38 @@
-import React from "react";
-import SSEComponent from "./SSETest";
+import React, { useEffect, useState } from "react";
+
 
 const TestPage = () => {
+  const [loc, setLoc] = useState(null);
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      const watchId = navigator.geolocation.watchPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+          setLoc(`Latitude: ${latitude}, Longitude: ${longitude}`);
+        },
+        (error) => {
+          console.error("Error watching position:", error);
+        }
+      );
+
+      return () => {
+        navigator.geolocation.clearWatch(watchId);
+      };
+    } else {
+      console.error("Geolocation is not supported by this browser.");
+    }
+
+  }, [])
+
   return (
     <div>
       <h1>This is a test</h1>
 
-      <SSEComponent />
+      <h3>Your location:</h3>
+
+      {loc}
+
     </div>
   );
 };
