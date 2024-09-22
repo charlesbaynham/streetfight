9933b3d9bb0904cd7c455ea18af66300ba1470c7
charlesbaynham <charlesbaynham@users.noreply.github.com>
2024-09-22 19:40:50 +0000

    Prettified Code!

--- react-ui/src/ShotQueue.js
+++ react-ui/src/ShotQueue.js
@@ -1,63 +1,84 @@
 import React, { useCallback, useEffect, useState } from "react";
 import { sendAPIRequest } from "./utils";
 
-
 function NearestPlayers({ shot_data }) {
-  if (shot_data === null)
-    return;
+  if (shot_data === null) return;
 
   const shooting_user_id = shot_data.user_id;
   const context = JSON.parse(shot_data.location_context);
 
   // Get user location in context array
-  const userIndex = context.findIndex((location) => location.user_id === shooting_user_id);
+  const userIndex = context.findIndex(
+    (location) => location.user_id === shooting_user_id,
+  );
   console.log("User index in context array:", userIndex);
 
   const shooting_user_data = context[userIndex];
   const shooting_user_latitude = shooting_user_data.latitude;
   const shooting_user_longitude = shooting_user_data.longitude;
 
-
   // Remove the user from the context array
-  const otherUsersContext = context.filter((location) => location.user_id !== shooting_user_id);
+  const otherUsersContext = context.filter(
+    (location) => location.user_id !== shooting_user_id,
+  );
   console.log("Updated context array:", otherUsersContext);
 
   // For each remaining player, calculate the distance from them to the shooting player
-  const shooting_users = otherUsersContext.map(({ user_id, team_id, user, team, latitude, longitude, state, timestamp }) => {
-    // Calculate distance between two points
-    const R = 6371e3; // metres
-    const φ1 = shooting_user_latitude * Math.PI / 180; // φ, λ in radians
-    const φ2 = latitude * Math.PI / 180;
-    const Δφ = (latitude - shooting_user_latitude) * Math.PI / 180;
-    const Δλ = (longitude - shooting_user_longitude) * Math.PI / 180;
+  const shooting_users = otherUsersContext.map(
+    ({
+      user_id,
+      team_id,
+      user,
+      team,
+      latitude,
+      longitude,
+      state,
+      timestamp,
+    }) => {
+      // Calculate distance between two points
+      const R = 6371e3; // metres
+      const φ1 = (shooting_user_latitude * Math.PI) / 180; // φ, λ in radians
+      const φ2 = (latitude * Math.PI) / 180;
+      const Δφ = ((latitude - shooting_user_latitude) * Math.PI) / 180;
+      const Δλ = ((longitude - shooting_user_longitude) * Math.PI) / 180;
 
-    const a = Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
-      Math.cos(φ1) * Math.cos(φ2) *
-      Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
-    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+      const a =
+        Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
+        Math.cos(φ1) * Math.cos(φ2) * Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
+      const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 
-    const distance = R * c; // in metres
+      const distance = R * c; // in metres
 
-    return { distance, user_id, team_id, user, team, latitude, longitude, state, timestamp };
-  }
+      return {
+        distance,
+        user_id,
+        team_id,
+        user,
+        team,
+        latitude,
+        longitude,
+        state,
+        timestamp,
+      };
+    },
   );
 
   // Sort shooting_users by distance
-  shooting_users.sort((a, b) => (a.distance > b.distance) ? 1 : -1);
+  shooting_users.sort((a, b) => (a.distance > b.distance ? 1 : -1));
 
-  return <>
-    <h3>Candidates:</h3>
-    <ul>
-      {shooting_users.map((user, idx) => (
-        <li key={idx}>
-          {user.user} - {user.team} - ({user.distance.toFixed(2)}m)
-        </li>
-      ))}
-    </ul>
-  </>
-
+  return (
+    <>
+      <h3>Candidates:</h3>
+      <ul>
+        {shooting_users.map((user, idx) => (
+          <li key={idx}>
+            {user.user} - {user.team} - ({user.distance.toFixed(2)}m)
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
-
 
 export default function ShotQueue() {
   const [shot, setShot] = useState(null);
@@ -72,7 +93,7 @@ export default function ShotQueue() {
         const newShot = data.shots[0];
         console.log("New shot:");
         console.dir(newShot);
-        console.log(JSON.parse(newShot.location_context))
+        console.log(JSON.parse(newShot.location_context));
         setShot(newShot);
       } else {
         setShot(null);
