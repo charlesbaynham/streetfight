2dbb89bde6ef06af45042e57a750647a1f27c7b3
Charles Baynham <charles.baynham@gmail.com>
2024-09-28 16:51:15 +0100

    Replace map, still fake coordinates (#44)
    * Replace map with real one (still low res)
    
    * Comments
    
    * Fake coordinates for testing today
    
    * Prettified Code!
    
    * Bump
    
    * Prettified Code!
    
    * Bump
    
    ---------
    
    Co-authored-by: charlesbaynham <charlesbaynham@users.noreply.github.com>

--- react-ui/src/MapView.js
+++ react-ui/src/MapView.js
@@ -2,20 +2,38 @@ import React, { useCallback, useEffect, useRef, useState } from "react";
 
 import { sendAPIRequest } from "./utils";
 
-import mapSrc from "./images/mapsample.png";
+import mapSrc from "./images/map_lowres.png";
 
 import styles from "./MapView.module.css";
 import Dot from "./Dot";
 
-// Top squiggly road tip to the star
+// Based on calculations and markup in "map alignment.svg"
+// const ref_map_width_px = 1188.5;
+// const ref_map_height_px = 1233.5;
+// const ref_1_lat_long = [51.4076739525208, -0.30754164680355806];
+// const ref_1_xy = [294.098, 963.464];
+// const ref_2_lat_long = [51.41383263398225, -0.30056843291595964];
+// const ref_2_xy = [825.823, 212.722];
+
+// const long_per_width_px = (ref_2_lat_long[1] - ref_1_lat_long[1]) / (ref_2_xy[0] - ref_1_xy[0]);
+// const lat_per_height_px = (ref_2_lat_long[0] - ref_1_lat_long[0]) / (ref_2_xy[1] - ref_1_xy[1]);
+// const map_bottom_left = {
+//   long: ref_1_lat_long[1] - ref_1_xy[0] * long_per_width_px,
+//   lat: ref_1_lat_long[0] + (ref_map_height_px - ref_1_xy[1]) * lat_per_height_px,
+// };
+// const map_top_right = {
+//   long: ref_1_lat_long[1] + (ref_map_width_px - ref_1_xy[0]) * long_per_width_px,
+//   lat: ref_1_lat_long[0] - ref_1_xy[1] * lat_per_height_px,
+// };
+
 const map_bottom_left = {
-  long: -2.741752117059972,
-  lat: 51.79127318746444,
+  lat: 51.39086326418569,
+  long: -0.32774517566164785,
 };
 
 const map_top_right = {
-  long: -2.734091728069985,
-  lat: 51.794378768913234,
+  lat: 51.57437959599135,
+  long: -0.001323774970900953,
 };
 
 const degreesLongitudePerKm =
@@ -146,11 +164,11 @@ function MapView({
     // Calculate our own dot
     const [dot_x, dot_y] = ownPosition
       ? coordsToPixels(
-        ownPosition.coords.latitude,
-        ownPosition.coords.longitude,
-        box_centre_lat,
-        box_centre_long,
-      )
+          ownPosition.coords.latitude,
+          ownPosition.coords.longitude,
+          box_centre_lat,
+          box_centre_long,
+        )
       : [0, 0];
 
     // Calculate all the other dots
@@ -230,10 +248,10 @@ function MapView({
             alwaysExpanded
               ? null
               : () => {
-                console.log("Click!");
-                setPoppedOut(!poppedOut);
-                handleResize();
-              }
+                  console.log("Click!");
+                  setPoppedOut(!poppedOut);
+                  handleResize();
+                }
           }
         ></div>
       </div>
