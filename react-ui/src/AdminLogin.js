8a417ce101a147ac8a827d25cad97a0d56bce25b
charlesbaynham <charlesbaynham@users.noreply.github.com>
2024-11-30 13:54:09 +0000

    Prettified Code!

--- react-ui/src/AdminLogin.js
+++ react-ui/src/AdminLogin.js
@@ -1,10 +1,7 @@
 import React, { useCallback, useRef } from "react";
 import { sendAPIRequest } from "./utils";
 
-
-
 export default function AdminLogin() {
-
   const login = useCallback((password) => {
     sendAPIRequest(
       "admin_authenticate",
@@ -14,9 +11,6 @@ export default function AdminLogin() {
       "POST",
     );
   }, []);
-
-
-
 
   const passwordInput = useRef(null);
 
