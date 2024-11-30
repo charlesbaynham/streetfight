e2d9611fd516a2b5e28d74338132168feebbef5e
Charles Baynham <charles.baynham@gmail.com>
2024-11-30 21:09:20 +0000

    Tidy (#56)
    * Format (will be undone...)
    
    * Put back firefox orientation fix
    
    * Back to refs
    
    * Revert "Back to refs"
    
    This reverts commit c54fff845f38f357e826417f36fcef924c3a683a.
    
    * This created a circular doom loop
    
    * Reapply "Back to refs"
    
    This reverts commit a46da6aa08e41dc815dbc196bbcf2a2abf90d21c.
    
    * Use refs, but always reinit camera on visibility change
    
    * Wow, Apple really hate developers
    
    * Sad times
    
    * Prettified Code!
    
    * Bump
    
    ---------
    
    Co-authored-by: charlesbaynham <charlesbaynham@users.noreply.github.com>

--- react-ui/src/MyWebcam.js
+++ react-ui/src/MyWebcam.js
@@ -1,7 +1,25 @@
 // Make my own replacement for react-webcam because it's broken on iOS. Argh!!
 
-import { useEffect, useRef, useCallback, useState } from "react";
-import { requestWebcamAccess } from "./utils";
+import { useEffect, useRef, useCallback } from "react";
+import useScreenOrientation from "./useScreenOrientation";
+
+const firefox = navigator.userAgent.toLowerCase().includes("firefox");
+
+function isIosSafari() {
+  const userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+  // Check for iOS (iPhone, iPad, iPod)
+  const isIosDevice =
+    /iPad|iPhone|iPod/.test(userAgent) ||
+    (navigator.platform === "MacIntel" && navigator.maxTouchPoints > 1);
+
+  // Check for Safari (and not Chrome or other browsers)
+  const isSafari = /Safari/.test(userAgent) && !/Chrome/.test(userAgent);
+
+  return isIosDevice && isSafari;
+}
+
+const safari = isIosSafari();
 
 const constraints = {
   audio: false,
@@ -16,7 +34,7 @@ export function MyWebcam({ trigger, className = "" }) {
   const canvasRef = useRef(null);
   const videoRef = useRef(null);
 
-  const [mediaStream, setMediaStream] = useState(null);
+  const mediaStream = useRef(null);
 
   // Define a function that will take a shot and upload it to the server
   const capture = useCallback(() => {
@@ -64,7 +82,7 @@ export function MyWebcam({ trigger, className = "" }) {
       const stream = await navigator.mediaDevices.getUserMedia(constraints);
       video.srcObject = stream;
       await video.play(); // This line is critical for making the camera resume from standby in Safari, and wasting an entire Saturday
-      setMediaStream(stream);
+      mediaStream.current = stream;
     } catch (e) {
       console.error("navigator.getUserMedia error:", e);
     }
@@ -72,11 +90,11 @@ export function MyWebcam({ trigger, className = "" }) {
 
   // Stop the camera
   const stopCamera = useCallback(() => {
-    if (mediaStream) {
-      mediaStream.getTracks().forEach((track) => track.stop());
-      setMediaStream(null);
+    if (mediaStream.current) {
+      mediaStream.current.getTracks().forEach((track) => track.stop());
+      mediaStream.current = null;
     }
-  }, [mediaStream, setMediaStream]);
+  }, [mediaStream]);
 
   // Connect the webcam to the <video> element on startup
   useEffect(() => {
@@ -86,27 +104,33 @@ export function MyWebcam({ trigger, className = "" }) {
 
     startCamera();
 
-    const video = videoRef.current;
-
     return () => {
       // Close down the camera streams when the component is unmounted
       stopCamera();
     };
-  }, [canvasRef, videoRef, capture, startCamera]);
+  }, [canvasRef, videoRef, capture, startCamera, stopCamera]);
 
-  document.addEventListener("visibilitychange", () => {
-    if (document.visibilityState === "visible") {
-      if (
-        !mediaStream ||
-        mediaStream.getVideoTracks().length === 0 ||
-        videoRef.current.readyState === 0
-      ) {
+  // Bugfix for Safari: Reinitialize the camera when the tab is hidden and then shown again
+  if (safari) {
+    document.addEventListener("visibilitychange", () => {
+      if (document.visibilityState === "visible") {
         console.log("Reinitializing camera...");
         stopCamera();
         startCamera();
       }
-    }
-  });
+    });
+  }
+
+  // Bugfix for Firefox: Reinitialize the camera when the screen orientation changes
+  // TODO: this isn't enough - I need to also rotate the camera apparently. See src for react-webcam I guess, or just don't bother
+  // const orientation = useScreenOrientation();
+  // useEffect(() => {
+  //     if (!firefox) return;
+  //     if (mediaStream.current) {
+  //         stopCamera();
+  //         startCamera();
+  //     }
+  // }, [orientation, mediaStream, startCamera, stopCamera]);
 
   return (
     <div className={className}>
