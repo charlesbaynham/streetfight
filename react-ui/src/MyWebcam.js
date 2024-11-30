fef38df07d0dc79a018eb851f4ca5c921e93d134
Charles Baynham <c.baynham@imperial.ac.uk>
2024-11-30 20:41:04 +0000

    Format (will be undone...)

--- react-ui/src/MyWebcam.js
+++ react-ui/src/MyWebcam.js
@@ -1,123 +1,123 @@
 // Make my own replacement for react-webcam because it's broken on iOS. Argh!!
 
 import { useEffect, useRef, useCallback, useState } from "react";
-import { requestWebcamAccess } from "./utils";
+
 
 const constraints = {
-  audio: false,
-  video: {
-    facingMode: "environment",
-    width: { ideal: 2048 },
-    height: { ideal: 1080 },
-  },
+    audio: false,
+    video: {
+        facingMode: "environment",
+        width: { ideal: 2048 },
+        height: { ideal: 1080 },
+    },
 };
 
 export function MyWebcam({ trigger, className = "" }) {
-  const canvasRef = useRef(null);
-  const videoRef = useRef(null);
+    const canvasRef = useRef(null);
+    const videoRef = useRef(null);
 
-  const [mediaStream, setMediaStream] = useState(null);
+    const [mediaStream, setMediaStream] = useState(null);
 
-  // Define a function that will take a shot and upload it to the server
-  const capture = useCallback(() => {
-    if (videoRef.current === null || canvasRef.current === null) {
-      return;
-    }
-    const video = videoRef.current;
-    const canvas = canvasRef.current;
+    // Define a function that will take a shot and upload it to the server
+    const capture = useCallback(() => {
+        if (videoRef.current === null || canvasRef.current === null) {
+            return;
+        }
+        const video = videoRef.current;
+        const canvas = canvasRef.current;
 
-    let w = video.videoWidth;
-    let h = video.videoHeight;
-    canvas.width = w;
-    canvas.height = h;
+        let w = video.videoWidth;
+        let h = video.videoHeight;
+        canvas.width = w;
+        canvas.height = h;
 
-    let ctx = canvas.getContext("2d");
+        let ctx = canvas.getContext("2d");
 
-    ctx.drawImage(video, 0, 0, w, h);
+        ctx.drawImage(video, 0, 0, w, h);
 
-    const imageSrc = canvas.toDataURL("image/jpeg");
+        const imageSrc = canvas.toDataURL("image/jpeg");
 
-    const query = JSON.stringify({
-      photo: imageSrc,
+        const query = JSON.stringify({
+            photo: imageSrc,
+        });
+
+        const requestOptions = {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: query,
+        };
+        fetch("/api/submit_shot", requestOptions)
+            .then((response) => response.json())
+            .then((data) => console.log(`Response: ${data}`));
+    }, []);
+
+    // Trigger the capture function when `trigger` changes
+    useEffect(() => {
+        if (trigger) capture();
+    }, [trigger, capture]);
+
+    // Start the camera and bind it to the <video> element
+    const startCamera = useCallback(async () => {
+        const video = videoRef.current;
+
+        try {
+            const stream = await navigator.mediaDevices.getUserMedia(constraints);
+            video.srcObject = stream;
+            await video.play(); // This line is critical for making the camera resume from standby in Safari, and wasting an entire Saturday
+            setMediaStream(stream);
+        } catch (e) {
+            console.error("navigator.getUserMedia error:", e);
+        }
+    }, []);
+
+    // Stop the camera
+    const stopCamera = useCallback(() => {
+        if (mediaStream) {
+            mediaStream.getTracks().forEach((track) => track.stop());
+            setMediaStream(null);
+        }
+    }, [mediaStream, setMediaStream]);
+
+    // Connect the webcam to the <video> element on startup
+    useEffect(() => {
+        if (videoRef.current === null) {
+            return;
+        }
+
+        startCamera();
+
+        const video = videoRef.current;
+
+        return () => {
+            // Close down the camera streams when the component is unmounted
+            stopCamera();
+        };
+    }, [canvasRef, videoRef, capture, startCamera]);
+
+    document.addEventListener("visibilitychange", () => {
+        if (document.visibilityState === "visible") {
+            if (
+                !mediaStream ||
+                mediaStream.getVideoTracks().length === 0 ||
+                videoRef.current.readyState === 0
+            ) {
+                console.log("Reinitializing camera...");
+                stopCamera();
+                startCamera();
+            }
+        }
     });
 
-    const requestOptions = {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: query,
-    };
-    fetch("/api/submit_shot", requestOptions)
-      .then((response) => response.json())
-      .then((data) => console.log(`Response: ${data}`));
-  }, []);
+    return (
+        <div className={className}>
+            <video
+                playsinline
+                muted // Needed for autoplay
+                style={{ width: "100%", height: "100%", objectFit: "cover" }}
+                ref={videoRef}
+            ></video>
 
-  // Trigger the capture function when `trigger` changes
-  useEffect(() => {
-    if (trigger) capture();
-  }, [trigger, capture]);
-
-  // Start the camera and bind it to the <video> element
-  const startCamera = useCallback(async () => {
-    const video = videoRef.current;
-
-    try {
-      const stream = await navigator.mediaDevices.getUserMedia(constraints);
-      video.srcObject = stream;
-      await video.play(); // This line is critical for making the camera resume from standby in Safari, and wasting an entire Saturday
-      setMediaStream(stream);
-    } catch (e) {
-      console.error("navigator.getUserMedia error:", e);
-    }
-  }, []);
-
-  // Stop the camera
-  const stopCamera = useCallback(() => {
-    if (mediaStream) {
-      mediaStream.getTracks().forEach((track) => track.stop());
-      setMediaStream(null);
-    }
-  }, [mediaStream, setMediaStream]);
-
-  // Connect the webcam to the <video> element on startup
-  useEffect(() => {
-    if (videoRef.current === null) {
-      return;
-    }
-
-    startCamera();
-
-    const video = videoRef.current;
-
-    return () => {
-      // Close down the camera streams when the component is unmounted
-      stopCamera();
-    };
-  }, [canvasRef, videoRef, capture, startCamera]);
-
-  document.addEventListener("visibilitychange", () => {
-    if (document.visibilityState === "visible") {
-      if (
-        !mediaStream ||
-        mediaStream.getVideoTracks().length === 0 ||
-        videoRef.current.readyState === 0
-      ) {
-        console.log("Reinitializing camera...");
-        stopCamera();
-        startCamera();
-      }
-    }
-  });
-
-  return (
-    <div className={className}>
-      <video
-        playsinline
-        muted // Needed for autoplay
-        style={{ width: "100%", height: "100%", objectFit: "cover" }}
-        ref={videoRef}
-      ></video>
-
-      <canvas ref={canvasRef} style={{ display: "none" }}></canvas>
-    </div>
-  );
+            <canvas ref={canvasRef} style={{ display: "none" }}></canvas>
+        </div>
+    );
 }
--- react-ui/src/WebcamView.js
+++ react-ui/src/WebcamView.js
@@ -1,44 +1,30 @@
-import { useEffect, useRef, useState } from "react";
+import { useRef } from "react";
 
 import QRParser from "./QRParser";
 
 import screenfillStyles from "./ScreenFillStyles.module.css";
 import styles from "./WebcamView.module.css";
-import useScreenOrientation from "./useScreenOrientation";
+
 import { MyWebcam } from "./MyWebcam";
 
 export default function WebcamView({ trigger, isDead }) {
   // Get a reference to the webcam element
   const webcamRef = useRef(null);
 
-  const [hackyHideWebcam, setHackyHideWebcam] = useState(false);
 
-  const orientation = useScreenOrientation();
-
-  // useEffect(() => {
-  //   // Toggle the webcam on screen rotation to force a reload
-  //   setHackyHideWebcam(true);
-  //   const id = setTimeout(() => {
-  //     setHackyHideWebcam(false);
-  //   }, 500);
-
-  //   return () => { clearTimeout(id); }
-  // }, [setHackyHideWebcam, orientation]);
 
   return (
     <>
-      {!hackyHideWebcam ? (
-        <MyWebcam
-          ref={webcamRef}
-          trigger={trigger}
-          className={
-            screenfillStyles.screenFill +
-            " " +
-            styles.webcamVideo +
-            (isDead ? " " + styles.dead : "")
-          }
-        />
-      ) : null}
+      <MyWebcam
+        ref={webcamRef}
+        trigger={trigger}
+        className={
+          screenfillStyles.screenFill +
+          " " +
+          styles.webcamVideo +
+          (isDead ? " " + styles.dead : "")
+        }
+      />
       <QRParser webcamRef={webcamRef} />
     </>
   );
