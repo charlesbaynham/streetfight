611abf78651de917b3ee2c5ea855f3653fd6b72a
Charles Baynham <c.baynham@imperial.ac.uk>
2024-12-01 13:58:44 +0000

    Too fiddly

--- tests/test_events.py
+++ tests/test_events.py
@@ -1,70 +1,58 @@
-import asyncio
-import json
-import logging
-from unittest.mock import AsyncMock
-from unittest.mock import patch
+# import asyncio
+# import json
+# import logging
+# from unittest.mock import AsyncMock
+# from unittest.mock import patch
 
-import pytest
+# import pytest
 
-from backend.sse_event_streams import SSE_KEEPALIVE_TIMEOUT
-from backend.sse_event_streams import admin_updates_generator
-from backend.sse_event_streams import make_sse_update_message
-from backend.sse_event_streams import updates_generator
+# from backend.sse_event_streams import SSE_KEEPALIVE_TIMEOUT
+# from backend.sse_event_streams import admin_updates_generator
+# from backend.sse_event_streams import make_sse_update_message
+# from backend.sse_event_streams import updates_generator
+# from backend.user_interface import UserInterface
+# from backend.model import Shot
 
-logger = logging.getLogger(__name__)
-
-
-@pytest.fixture
-def mock_user_interface():
-    with patch("backend.sse_event_streams.UserInterface") as MockUserInterface:
-        yield MockUserInterface
+# logger = logging.getLogger(__name__)
 
 
-@pytest.fixture
-def mock_admin_interface():
-    with patch("backend.sse_event_streams.AdminInterface") as MockAdminInterface:
-        yield MockAdminInterface
+# @pytest.fixture
+# def mock_user_interface():
+#     with patch("backend.sse_event_streams.UserInterface") as MockUserInterface:
+#         yield MockUserInterface
 
 
-def test_make_sse_update_message():
-    message = {"handler": "update_prompt", "data": "user"}
-    expected_output = f"data: {json.dumps(message)}\n\n"
-    assert make_sse_update_message(json.dumps(message)) == expected_output
+# @pytest.fixture
+# def mock_admin_interface():
+#     with patch("backend.sse_event_streams.AdminInterface") as MockAdminInterface:
+#         yield MockAdminInterface
 
 
-@pytest.mark.asyncio
-async def test_updates_generator(mock_user_interface):
-    user_id = 1
-    mock_user_interface.return_value.generate_user_updates = AsyncMock(
-        return_value=iter([None])
-    )
-    mock_user_interface.return_value.get_team_id = AsyncMock(return_value=None)
+# @pytest.mark.asyncio
+# async def test_ticker_announces_kill(
+#     db_session,
+#     api_client,
+#     admin_api_client,
+#     api_user_id,
+#     team_factory,
+#     test_image_string,
+# ):
+#     # Put a user in a team and have them take a shot
+#     UserInterface(api_user_id).join_team(team_factory())
+#     UserInterface(api_user_id).award_ammo(1)
+#     UserInterface(api_user_id).submit_shot(test_image_string)
+#     shot_a = db_session.query(Shot.id).order_by(Shot.id.desc()).first()[0]
 
-    gen = updates_generator(user_id)
-    result = [await anext(gen) for _ in range(2)]
-    assert len(result) == 2
+#     # Subscribe to the ticker update queue for this user
+#     ticker_generator = UserInterface(api_user_id).generate_ticker_updates()
 
+#     # Read one message to make sure the event is subscribed to
+#     m = await anext(ticker_generator)
 
-@pytest.mark.asyncio
-async def test_admin_updates_generator(mock_admin_interface):
-    mock_admin_interface.return_value.generate_any_ticker_updates = AsyncMock(
-        return_value=iter([None])
-    )
+#     # Let's say the user shot themselves:
+#     response = await admin_api_client.post(
+#         f"/api/admin_shot_hit_user?shot_id={shot_a}&target_user_id={api_user_id}"
+#     )
+#     assert response.is_success
 
-    gen = admin_updates_generator()
-    result = [await anext(gen) for _ in range(2)]
-    assert len(result) == 2
-
-
-@pytest.mark.asyncio
-async def test_keepalive_timer():
-    async def keepalive_timer():
-        i = 0
-        while True:
-            await asyncio.sleep(SSE_KEEPALIVE_TIMEOUT)
-            yield i
-            i += 1
-
-    gen = keepalive_timer()
-    result = [await anext(gen) for _ in range(2)]
-    assert result == [0, 1]
+#     # Check that the ticker announces the kill
