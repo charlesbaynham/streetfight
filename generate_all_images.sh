5e921deb1644771c5b266a2cb588e61b888c57d4
Charles Baynham <c.baynham@imperial.ac.uk>
2024-12-01 21:00:52 +0000

    Generation script

--- generate_all_images.sh
+++ generate_all_images.sh
@@ -1,3 +1,73 @@
 
-npm run qrgen -- --type ammo --num 3 --tag ammotest
-npm run qrgen -- --type armour --num 2 --tag armourtest
+# Ammo
+
+npm run qrgen -- --type ammo --num 1 --tag ammo_a
+npm run qrgen -- --type ammo --num 1 --tag ammo_b
+npm run qrgen -- --type ammo --num 1 --tag ammo_c
+npm run qrgen -- --type ammo --num 1 --tag ammo_d
+npm run qrgen -- --type ammo --num 1 --tag ammo_e
+npm run qrgen -- --type ammo --num 1 --tag ammo_3f
+npm run qrgen -- --type ammo --num 1 --tag ammo_g
+npm run qrgen -- --type ammo --num 1 --tag ammo_h
+npm run qrgen -- --type ammo --num 1 --tag ammo_i
+npm run qrgen -- --type ammo --num 1 --tag ammo_j
+
+
+npm run qrgen -- --type ammo --num 3 --tag ammo_a
+npm run qrgen -- --type ammo --num 3 --tag ammo_b
+npm run qrgen -- --type ammo --num 3 --tag ammo_c
+npm run qrgen -- --type ammo --num 3 --tag ammo_d
+
+npm run qrgen -- --type ammo --num 5 --tag ammo_a
+npm run qrgen -- --type ammo --num 5 --tag ammo_b
+npm run qrgen -- --type ammo --num 5 --tag ammo_c
+npm run qrgen -- --type ammo --num 5 --tag ammo_d
+npm run qrgen -- --type ammo --num 5 --tag ammo_e
+
+
+# Armour
+
+npm run qrgen -- --type armour --num 1 --tag armour_lv1_a
+npm run qrgen -- --type armour --num 1 --tag armour_lv1_b
+npm run qrgen -- --type armour --num 1 --tag armour_lv1_c
+npm run qrgen -- --type armour --num 1 --tag armour_lv1_d
+npm run qrgen -- --type armour --num 1 --tag armour_lv1_e
+npm run qrgen -- --type armour --num 1 --tag armour_lv1_f
+
+
+npm run qrgen -- --type armour --num 2 --tag armour_lv2_a
+npm run qrgen -- --type armour --num 2 --tag armour_lv2_b
+npm run qrgen -- --type armour --num 2 --tag armour_lv2_c
+npm run qrgen -- --type armour --num 2 --tag armour_lv2_d
+
+npm run qrgen -- --type armour --num 3 --tag armour_lv3_a
+npm run qrgen -- --type armour --num 3 --tag armour_lv3_b
+
+# Level guns
+
+npm run qrgen -- --type weapon --num 1 --damage 1 --timeout 6 --tag weapon_lv1_a
+npm run qrgen -- --type weapon --num 1 --damage 1 --timeout 6 --tag weapon_lv1_b
+npm run qrgen -- --type weapon --num 1 --damage 1 --timeout 6 --tag weapon_lv1_c
+npm run qrgen -- --type weapon --num 1 --damage 1 --timeout 6 --tag weapon_lv1_d
+npm run qrgen -- --type weapon --num 1 --damage 1 --timeout 6 --tag weapon_lv1_e
+npm run qrgen -- --type weapon --num 1 --damage 1 --timeout 6 --tag weapon_lv1_f
+npm run qrgen -- --type weapon --num 1 --damage 1 --timeout 6 --tag weapon_lv1_g
+
+
+npm run qrgen -- --type weapon --num 1 --damage 2 --timeout 6 --tag weapon_lv2_a
+npm run qrgen -- --type weapon --num 1 --damage 2 --timeout 6 --tag weapon_lv2_b
+npm run qrgen -- --type weapon --num 1 --damage 2 --timeout 6 --tag weapon_lv2_c
+npm run qrgen -- --type weapon --num 1 --damage 2 --timeout 6 --tag weapon_lv2_d
+
+
+npm run qrgen -- --type weapon --num 1 --damage 3 --timeout 6 --tag weapon_lv3_a
+npm run qrgen -- --type weapon --num 1 --damage 3 --timeout 6 --tag weapon_lv3_b
+npm run qrgen -- --type weapon --num 1 --damage 3 --timeout 6 --tag weapon_lv3_c
+npm run qrgen -- --type weapon --num 1 --damage 3 --timeout 6 --tag weapon_lv3_d
+
+# Medpack
+
+
+npm run qrgen -- --type medpack --num 1 --tag medpack_a
+npm run qrgen -- --type medpack --num 1 --tag medpack_b
+npm run qrgen -- --type medpack --num 1 --tag medpack_c
