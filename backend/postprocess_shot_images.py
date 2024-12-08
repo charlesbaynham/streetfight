8acd70bd89f06f0b29c42d08e534bd08fbfeeecc
Charles Baynham <c.baynham@imperial.ac.uk>
2024-12-08 13:51:53 +0000

    Spot mistake

--- backend/postprocess_shot_images.py
+++ backend/postprocess_shot_images.py
@@ -1,4 +1,5 @@
 import logging
+import os
 from pathlib import Path
 
 from .admin_interface import AdminInterface
@@ -6,7 +7,7 @@ from .dotenv import load_env_vars
 from .image_processing import save_image
 from .model import ShotModel
 
-POSTPROCESS_OUTPUT_DIR = Path(__file__, "../../processed_shots").resolve()
+DEFAULT_POSTPROCESS_OUTPUT_DIR = Path(__file__, "../../processed_shots").resolve()
 
 logger = logging.getLogger(__name__)
 
@@ -18,6 +19,10 @@ def output_images():
     Outputs all shot images with markup
     """
 
+    output_dir = os.getenv(
+        "POSTPROCESS_OUTPUT_DIR", str(DEFAULT_POSTPROCESS_OUTPUT_DIR)
+    )
+
     for shot_id in AdminInterface().get_all_shot_ids():
         shot_model: ShotModel = AdminInterface().get_shot_model(shot_id=shot_id)
 
@@ -28,7 +33,7 @@ def output_images():
         save_image(
             base64_image=marked_up_shot.image_base64,
             name=marked_up_shot.user.name,
-            output_dir=POSTPROCESS_OUTPUT_DIR,
+            output_dir=output_dir,
         )
 
 
